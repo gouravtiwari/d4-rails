0682392d79355efd4ed37a32319e9df22009e688
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-02-24 23:01:53 +0530

    Basic files for d4-rails gem :trumpet

---
+++ lib/d4-rails/version.rb
@@ -0,0 +1,11 @@
+module D4Rails
+  # Returns the version of the currently loaded Contineo as a Gem::Version
+  def self.version
+    Gem::Version.new "0.0.1"
+  end
+
+  module VERSION #:nodoc:
+    MAJOR, MINOR, TINY = D4Rails.version.segments
+    STRING = D4Rails.version.to_s
+  end
+end
