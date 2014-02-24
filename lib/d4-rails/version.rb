a8eaeb2f2fad6d7bd6c6d58abf0a96ac2d74fc68
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-02-24 13:46:36 -0500

    Typo in repo name

--- lib/d4-rails/version.rb
+++ lib/d4-rails/version.rb
@@ -1,7 +1,7 @@
 module D4Rails
   # Returns the version of the currently loaded Contineo as a Gem::Version
   def self.version
-    Gem::Version.new "0.0.1"
+    Gem::Version.new "0.0.2"
   end
 
   module VERSION #:nodoc:
